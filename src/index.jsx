8264e34c44254019ec1d81532a46057257f9c5c2
Irvin Reyes <reyes.yerp@gmail.com>
2020-10-15 13:36:16 -0500

    Move redux store to its own folder

---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+import App from './components/App'
+import middleware from './middleware'
+import reducer from './reducers'
+
+const store = createStore(reducer, middleware)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+)
